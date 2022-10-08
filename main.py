e3eacca48a54fc68118c71513c215078f3d8f11a
Lerone Joyner <102872162+leronewjoyner@users.noreply.github.com>
2022-10-08 19:36:01 +0000

    Testing API key to TSA wait time

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import requests
-
 from datetime import datetime
-
-now = datetime.now()
-
-current_time = now.strftime("%H:%M:%S")
-
-print(data['estimated_hourly_times'])
-print(now)
 
 def getWaitTimes():
 	response = requests.get("https://www.tsawaittimes.com/api/airport/Izu2gTMXZEPT9cvoE2PrOCfxwmFyIgZc/ATL")
 	data = response.json()
 	return data
+
+def getCurrTime():
+	now = datetime.now()
+	current_time = now.strftime("%H:%M:%S")
+	return current_time
