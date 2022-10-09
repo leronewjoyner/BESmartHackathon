f5a69d1bb059291d7b9238b7a9bc53c8aaef83c5
Lerone Joyner <lerone.joyner.711@gmail.com>
2022-10-09 01:08:55 -0500

    added flight engine api

---
+++ python/flight_engine_API.py
@@ -0,0 +1,11 @@
+import requests
+import json
+
+def getFlightInfo(date,flightnum):
+    baseUrl = "https://morgan-knights-flight-engine.herokuapp.com/flights?"
+    url = baseUrl + ("date=" + date) + ("&flightNumber="+flightnum)
+    response = requests.get(url)
+    data = response.json()
+    print(data)
+
+getFlightInfo('2022-10-12','3301')
